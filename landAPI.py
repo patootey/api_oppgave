562e60f793998a1be99c6139470bbb5b63e75da1
Vobobos <vebjorn.landmark@outlook.com>
2024-01-26 15:16:42 +0100

    Gjorde landet funksjonsbasert

--- landAPI.py
+++ landAPI.py
@@ -2,16 +2,23 @@ import requests
 
 countries = requests.get("https://restcountries.com/v3.1/all")
 countries = countries.json()
-print(countries[0]['name']['official'])
-for i in countries[10]['name']:
-    if i == "nativeName":
-        for x in countries[10]['name'][i]:
-            if "Norway" in countries[10]['name'][i][x]:
-                print(x)
-    if "Norway" in countries[10]['name'][i]:
-        print(countries[10]['name'][i])
-    
-# print(countries[10]['name'])
-# print(countries[10]['currencies'])
-# for i in countries[10]['currencies']:
-#     print(i)
+def search(key:str):
+    for number in range(len(countries)):
+        
+        for i in countries[number]['name']:
+            if i == "nativeName":
+                for x in countries[number]['name'][i]:
+                    for z in countries[number]['name'][i][x]:
+                        if key in countries[number]['name'][i][x][z]:
+                            for name in countries[number]['currencies']:
+                                z = name
+                            return countries[number]['name']['official'], z
+                    
+                    
+            if key in countries[number]['name'][i]:
+                for name in countries[number]['currencies']:
+                    z = name
+                return countries[number]['name']['official'], z
+print(search("Norway"))
+print(search("Norge"))
+print(search("Noreg"))
