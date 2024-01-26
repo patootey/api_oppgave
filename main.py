ad4cc8d8c5984e0e7f026831616477467a09fdc7
vobobos <vebjorn.landmark@outlook.com>
2024-01-26 10:24:49 +0100

    la til daarlig eksempel på noe en kan gjoere uten kommentarer

--- main.py
+++ main.py
@@ -3,8 +3,17 @@ import requests
 
 currencies = requests.get("https://api.frankfurter.app/currencies")
 key = "https://api.frankfurter.app/"
-addon = "latest"
-request = requests.get(key+addon)
+
+current = "latest"
+historical = "2008-09-15"
+fromDate,toDate = "2008-09-15..", "2017-09-15"
+query = False
+fromCurrency = "from=EUR"
+to = "to=USD"
+
+request = requests.get(key+current)
 request = request.json()
 print(f"I datoen {request['date']} hadde valutaen {currencies.json()['USD']} en verdi på {request['rates']['USD']} i {request['base']}")
 # print(currencies.json()["AUD"])
+newkey = requests.get(key+fromDate+toDate+"?"+fromCurrency+"&"+to)
+print(newkey.json())
