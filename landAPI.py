cb17810d43cea63421c043454bdee26cfa092d7b
Vobobos <vebjorn.landmark@outlook.com>
2024-01-26 16:30:38 +0100

    la til en graf i main men som gjeor at den ikke funker om du bare har for en dato. landAPI ble ryddet opp i og printer ut naa det vi trenger

--- landAPI.py
+++ landAPI.py
@@ -3,22 +3,26 @@ import requests
 countries = requests.get("https://restcountries.com/v3.1/all")
 countries = countries.json()
 def search(key:str):
-    for number in range(len(countries)):
-        
-        for i in countries[number]['name']:
+    for country in countries:
+            
+        for i in country['name']:
+            if key in country['name'][i]:
+                for name in country['currencies']:
+                    symbol = name
+                return country['name']['common'], symbol, country['currencies'][symbol]["symbol"]
+                
+
             if i == "nativeName":
-                for x in countries[number]['name'][i]:
-                    for z in countries[number]['name'][i][x]:
-                        if key in countries[number]['name'][i][x][z]:
-                            for name in countries[number]['currencies']:
-                                z = name
-                            return countries[number]['name']['official'], z
+                for x in country['name'][i]:
+                    for z in country['name'][i][x]:
+                        if key in country['name'][i][x][z]:
+                            for name in country['currencies']:
+                                symbol = name
+                            return country['name']['common'], symbol, country['currencies'][symbol]["symbol"]
+    return f"Fant ikke land {key}"
                     
                     
-            if key in countries[number]['name'][i]:
-                for name in countries[number]['currencies']:
-                    z = name
-                return countries[number]['name']['official'], z
 print(search("Norway"))
 print(search("Norge"))
-print(search("Noreg"))
+print(search("Britain"))
+print(search("Lugudo"))
