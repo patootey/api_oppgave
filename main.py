8973be8e37444a749c5e0c07c7779354bf75122a
vobobos <vebjorn.landmark@outlook.com>
2024-01-26 10:02:42 +0100

    skrevet ut et eksempel paa hva som en kan gjoere med API

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import tkinter
 import requests
 
-response = requests.get("https://api.frankfurter.app/currencies")
-
-print(response.json()["AUD"])
+currencies = requests.get("https://api.frankfurter.app/currencies")
+key = "https://api.frankfurter.app/"
+addon = "latest"
+request = requests.get(key+addon)
+request = request.json()
+print(f"I datoen {request['date']} hadde valutaen {currencies.json()['USD']} en verdi på {request['rates']['USD']} i {request['base']}")
+# print(currencies.json()["AUD"])
