e1dede02290c66edbeb62c1b8516c29869844941
Grocto <groctoz@gmail.com>
2024-01-30 09:35:03 +0100

    litt mer

--- front_page.py
+++ front_page.py
@@ -32,7 +32,7 @@ def main(root):
     name1_entry = tk.Entry(root).grid(row=3, column=1)
     name2_entry = tk.Entry(root).grid(row=3, column=2)
 
-    search = ge.Button(root, {"text":"Start Søk"}, command=rp.results(root))
+    search = ge.Button(root, {"text":"Start Søk"}, command=lambda: rp.results(root))
     search.button.grid(row=4,column=1)
     root.mainloop()  # Starter GUI-hovedløkka for å vise vinduet
 
