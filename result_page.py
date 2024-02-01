73d532f06158997c4b199f79a10f5920a9ac5588
Grocto <groctoz@gmail.com>
2024-02-01 13:31:05 +0100

    ENADA MER

--- result_page.py
+++ result_page.py
@@ -1,19 +1,25 @@
 import tkinter as tk
 import general as ge
 import landAPI as la
-import requests
-from PIL import Image, ImageTk
-from io import BytesIO
 
-def results(root, key):
-    frame = tk.Frame(root)
-    image_url = la.search(key)[3]
-    response = requests.get(image_url)
-    if response.status_code == 200:
-        image_data = response.content
-        image = Image.open(BytesIO(image_data))
-        photo_image = ImageTk.PhotoImage(image)
-        label = tk.Label(root, image=photo_image)
-        label.image = photo_image  # Keep a reference to prevent garbage collection
-        label.place(x=0, y=0)
-    frame.place(y=50)
+def results(root, key1, key2, country):
+    ge.prevpage = ge.keep_page(root)
+    frame = tk.Frame(root, bg='red', width=root.winfo_width(), height=root.winfo_height())  # Set a background color and dimensions for visibility
+    frame.place(y=50)  # Adjust coordinates as needed
+    try:
+        if country:
+            image_url1 = la.search(key1)[3]
+            image_url2 = la.search(key2)[3]
+
+            flag1 = ge.Photo(frame, image_path=image_url1)
+            flag1.label.place(x=0, y=0)
+
+            flag2 = ge.Photo(frame, image_path=image_url2)
+            flag2.label.place(x=100, y=0)
+            print("HABSAD")
+
+    except Exception as e:
+        ge.load_page(root, ge.prevpage)
+        print("Error occurred:", str(e))
+        print(la.search(key1))
+        print(la.search(key2))
