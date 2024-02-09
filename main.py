6c9290c4de4d5bb6de4d69a18dd74e45eca01774
vobobos <vebjorn.landmark@outlook.com>
2024-02-09 13:28:31 +0100

    la til docstrings

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-import tkinter
 import requests
 from matplotlib import pyplot as plt
 
-countries = requests.get("https://restcountries.com/v3.1/all")
-countries = countries.json()
 def search(key:str):
+    """
+    Funksjon for å søke gjennom rescountries API der den returnerer landets navn, valutanavn, 
+    -kode og -symbol, og en PNG av landets flagg
+
+    Parametre:
+        key (string): Nøkkelen brukeren oppgir for å søke i datasettet
+    """
+    countries = requests.get("https://restcountries.com/v3.1/all")
+    countries = countries.json()
+
     for country in countries:
             
         for code in country['name']:
@@ -34,27 +41,59 @@ def search(key:str):
 
     return f"Fant ikke landet '{key}'."
 
+
 class Currency():
+    """
+    Klasse for å la brukeren sette sammen en forespørsel, hente data fra valuta API
+    og lage en graf med valutaens endring gjennom tid. 
+
+    Attributt:
+        key (string): første delen av forespørselen til API-et
+    """
     def __init__(self):
         self.key = "https://api.frankfurter.app/"
     
     def chooser(self):
+        """
+        Metode som lar brukeren oppgi det som skal søkes etter i search funksjonen
+
+        Attributter:
+            fromCurrency (list): liste som inneholder de returnerte verdiene til search()
+            toCurrency1  (list): samme som over
+            toCurrency2  (list): samme som over
+
+        Returner: 
+            en string som skal legges til key
+        """
         self.fromCurrency = search(input("Gi land fra: "))
         self.toCurrency1 = search(input("Gi land til 1: "))
         self.toCurrency2 = search(input("Gi land til 2: "))
 
-
-
         return "?from="+self.fromCurrency[2]+"&to="+self.toCurrency1[2]+","+self.toCurrency2[2]
 
     def set_date(self):
+        """
+        Metode som bruker input til å sette et tidsintervall for sammenlikningen
+
+        Attributter:
+            startDate (string): starten på intervallet som skrives i formatet slik vist
+            endDate   (string): slutten på intervallet, der kun .. gir verdier til dags dato
+        
+        Returnerer:
+            En sammenslåing av de to strengene over som skal legges til i key
+        """
         self.startDate = input("Start (YYYY-MM-DD): ")
         self.endDate = input("End (..YYYY-MM-DD): ")
         return self.startDate + self.endDate
 
 
     def fetch_data(self):
-        print("HVAAAA")
+        """
+        Metode som slår sammen key og ber om dataen fra API-et
+
+        Returnerer:
+            Dataen fra API i json format som tilsvarer en dictionary i Python
+        """
         self.key += self.set_date() + self.chooser()
         print(self.key)
         self.key = requests.get(self.key)
@@ -62,6 +101,18 @@ class Currency():
     
     
     def list_maker(self, data):
+        """
+        Metode der dataen fra API-et blir satt inn i to lister som senere skal plottes
+        Dersom det oppstår en feil med listene så returnerer metoden en string som bearbeides i neste metode
+        Hvis ikke legger den inn alle mulige verdier i to lister
+        
+        Parametre:
+            data (list): en parameter som du kan gi en bestemt verdi eller la være for å få data fra fetch_data
+
+        Returnerer:
+            Enten listene
+            Eller en string
+        """
         try:
             raw_data = data
             if data == None:
@@ -80,9 +131,18 @@ class Currency():
             return e
 
     def plotter(self, data=None):
+        """
+        Metode som bruker listene tidligere definert, gitt at det fungerte, og plotter det
+        Dersom dataen er en string stopper metoden og erroren skal behandles
+
+        Returnerer:
+            Den plottede figuren som skal bearbeides i tkinter, fig
+            Dersom det er noen feilmeldinger befinner de seg i listen error
+        """
         data = self.list_maker(data)
         if type(data) == str:
             return data
+        
         fig, ax = plt.subplots()
         ax.grid()
         ax.plot(data[0], color="blue")
