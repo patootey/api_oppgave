4d0d9899f48a0ed75321b7069a7d0a2d8ae9b133
Grocto <groctoz@gmail.com>
2024-02-15 13:03:29 +0100

    👱‍♂️

--- front_page.py
+++ front_page.py
@@ -15,6 +15,15 @@ def default(buttonA, buttonB, label1, label2):
     label1.config(text=str(buttonB.settings["text"])+" A")
     label2.config(text=str(buttonB.settings["text"])+" B")
 
+
+def manual(root):
+    ge.prevpage = ge.keep_page(root)
+    frame = tk.Frame(root, width=root.winfo_width(), height=root.winfo_height()) 
+    frame.place(y=50)
+
+    help_image = ge.Photo(frame, image_path="./images/brukerveiledning.png", height=50)
+    help_image.label.grid(row=1, column=1)
+
 def main(root):
     ge.clear_window(root)  # Kaller en funksjon for å fjerne alle widgets fra vinduet
     frame = tk.Frame(root, width=root.winfo_width(), height=root.winfo_height()) 
@@ -62,7 +71,7 @@ def main(root):
     search = ge.Button(frame, {"text":"Start Søk"}, command=lambda: rp.results(root,name1_entry.get(), name2_entry.get(), name3_entry.get(), button1.clicked, start_date__entry.get(), end_date_entry.get()))
     search.button.grid(row=6,column=1)
 
-    help_button = ge.Button(frame, {"text": "HJELP"}, command=lambda: "SDJAD")
+    help_button = ge.Button(frame, {"text": "HJELP"}, command=lambda: manual(root))
     help_button.button.grid(row=6, column=3)
 
 
